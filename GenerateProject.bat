6255ea286127f95b819d9f7966a23c24b96e1ec7
Christoffer Persson <chripers@playgroundsquad.com>
2022-10-11 11:54:14 +0200

    added VulkanSDK check and updated to VS 2022

--- GenerateProject.bat
+++ GenerateProject.bat
@@ -1,1 +1,8 @@
-call Premake5\Premake5.exe vs2019
+@echo off
+if not exist "C:\VulkanSDK\" (
+  echo It seams that you don't have the Vulkan SDK installed. && echo Please download it at: https://vulkan.lunarg.com/
+  PAUSE
+)
+
+
+call Premake5\Premake5.exe vs2022
